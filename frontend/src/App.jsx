4db567b24f9b97636386387142b36bbb14454c39
taj-u-LW <tajcsebsmrstu@gmail.com>
2025-03-19 07:34:01 +0600

    initialize and setup project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h4>HandsOn - A Community-Driven Social Volunteering Platform</h4>
+      </div>
+    </>
+  );
+}
+
+export default App
